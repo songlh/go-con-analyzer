88590761a8804325fed87df2178ab271af951823
= <=>
2018-12-15 23:01:30 -0500

    rm blank line in scripts/read_result.py

--- scripts/read_result.py
+++ scripts/read_result.py
@@ -12,9 +12,9 @@ def rm_comment(file_path):
 	with open(file_path) as f:
 		lines = f.readlines()
 		for line in lines:
-			if not line.startswith("###"):
+			if line.strip() != "" and not line.startswith("###") and not line.startswith("Fail to find main package"):
 				print(line, end="")
-
+				
 
 if __name__ == "__main__":
 	rm_comment(sys.argv[1])
